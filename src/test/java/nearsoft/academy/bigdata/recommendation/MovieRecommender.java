43d7dcb10f2769cfdc91b64f1f645cc8f87fbbd1
Alonso Gutierrez <agutierrez@nearsoft.com>
2019-10-09 12:34:23 -0600

    Refactor based on feedback

--- src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
+++ src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
@@ -19,63 +19,64 @@ import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 public class MovieRecommender {
     private String file;
-    private Hashtable<String, Integer> HashProduct = new Hashtable<String, Integer>();
-    private Hashtable<Integer, String> InvertedHashProduct = new Hashtable<Integer, String>();
-    private Hashtable<String, Integer> HashUser = new Hashtable<String, Integer>();
-    private int users =0, products =0, reviews = 0;
+    private HashMap<String, Integer> HashProduct = new HashMap<String, Integer>();
+    private HashMap<Integer, String> InvertedHashProduct = new HashMap<Integer, String>();
+    private HashMap<String, Integer> HashUser = new HashMap<String, Integer>();
+    private int users = 0, products = 0, reviews = 0;
 
     public MovieRecommender(String file) throws IOException{
         this.file = file;
         getData();
     }
 
-    public String getData() throws IOException {
+    private void getData() throws IOException {
         int thisProduct =0, thisUser =0;
         Files.deleteIfExists(Paths.get("Result.csv"));
         File result = new File("Result.csv");
         InputStream fileReader = new GZIPInputStream(new FileInputStream(this.file));
-        BufferedReader br = new BufferedReader(new InputStreamReader(fileReader));
+        //BufferedReader br = new BufferedReader(new InputStreamReader(fileReader));
         FileWriter fileWriter = new FileWriter(result);
-        BufferedWriter bw = new BufferedWriter(fileWriter);
+        //BufferedWriter bw = new BufferedWriter(fileWriter);
         String line;
         String[] sp;
         String key, value;
 
-        while((line = br.readLine()) != null) {
-            if (line.length() >= 0) {
-                sp = line.split(" ");
-                key = sp[0];
-                if (key.equals("product/productId:")) {
-                    value = sp[1];
-                    if (!HashProduct.containsKey(value)){
-                        HashProduct.put(value,products);
-                        InvertedHashProduct.put(products,value);
-                        thisProduct = HashProduct.get(value);
-                        products++;
-                    }else{
-                        thisProduct = HashProduct.get(value);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(fileReader));
+             BufferedWriter bw = new BufferedWriter(fileWriter)) {
+            System.out.println("Reading File: "+this.file);
+            while((line = br.readLine()) != null) {
+                if (line.length() >= 0) {
+                    sp = line.split(":");
+                    key = sp[0];
+                    if ("product/productId".equals(key)) {
+                        value = sp[1].trim();
+                        if (!HashProduct.containsKey(value)){
+                            HashProduct.put(value,products);
+                            InvertedHashProduct.put(products,value);
+                            thisProduct = HashProduct.get(value);
+                            products++;
+                        }else{
+                            thisProduct = HashProduct.get(value);
+                        }
+                    }else if ("review/userId".equals(key)){
+                        value = sp[1].trim();
+                        if (!HashUser.containsKey(value)){
+                            HashUser.put(value, users);
+                            thisUser = HashUser.get(value);
+                            users++;
+                        }else{
+                            thisUser = HashUser.get(value);
+                        }
+                    }else if ("review/score".equals(key)){
+                        String score = sp[1].trim();
+                        bw.write(thisUser + "," + thisProduct + "," + score + "\n");
+                        reviews ++;
                     }
-                }else if (key.equals("review/userId:")){
-                    value = sp[1];
-                    if (!HashUser.containsKey(value)){
-                        HashUser.put(value, users);
-                        thisUser = HashUser.get(value);
-                        users++;
-                    }else{
-                        thisUser = HashUser.get(value);
-                    }
-                }else if (key.equals("review/score:")){
-                    String score = sp[1];
-                    bw.write(thisUser + "," + thisProduct + "," + score + "\n");
-                    reviews ++;
                 }
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        br.close();
-        bw.close();
-        return null;
-
-
     }
 
     public int getTotalReviews() {
@@ -98,7 +99,7 @@ public class MovieRecommender {
 
         int userValue = HashUser.get(user);
 
-        List RecommendedProducts = new ArrayList<String>();
+        List RecommendedProducts = new ArrayList<String>(3);
         List<RecommendedItem> recommendations = recommender.recommend(userValue,3);
         for (RecommendedItem recommendation : recommendations) {
             RecommendedProducts.add(InvertedHashProduct.get((int)recommendation.getItemID()));
